840993bbdc65b1589b704c407736f3a51c263790
Davi A Alves <daviih2000@gmail.com>
2023-05-02 17:19:17 -0300

    Adicionado o arquivo script e linkado com o arquivo index.

---
+++ script.js
@@ -0,0 +1,23 @@
+let logo = document.getElementById("logo");
+console.log(logo);
+
+let postAutores = document.getElementsByClassName("post-autor");
+console.log(postAutores);
+
+let post02 = document.getElementById("post02");
+console.log(post02);
+
+let form = document.getElementById("formulario");
+console.log(form);
+
+let posts = document.getElementById("posts");
+console.log(posts);
+
+let postsdatas = document.getElementsByClassName("post-data");
+console.log(postsdatas);
+
+let poststexts = document.getElementsByClassName("post-texto");
+console.log(poststexts);
+
+let socialMediasLinks = document.getElementsByClassName("lista_redes");
+console.log(socialMediasLinks);
