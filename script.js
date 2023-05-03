5758c49c96beaa8d9d4cad679a0838d1c576418d
Davi A Alves <daviih2000@gmail.com>
2023-05-03 11:24:04 -0300

    Adicionadas linhas no arquivo script.js exercitando o uso de querySelector.

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ console.log(poststexts);
 
 let socialMediasLinks = document.getElementsByClassName("lista_redes");
 console.log(socialMediasLinks);
+
+let texto02 = document.querySelector("#post02 .post-texto");
+console.log(texto02);
+
+let footerListRedes = document.querySelectorAll("footer .lista_redes li ")
+console.log(footerListRedes);
+
+let textLinkPost01 = document.querySelector("#post01 .post-texto a");
+console.log(textLinkPost01);
+
+let textStrongWordPost02 = document.querySelector("#post02 .post-texto strong");
+console.log(textStrongWordPost02);
+
+let formNameInput = document.querySelector("#formulario form div #nome");
+console.log(formNameInput);
+
+let navListLinks = document.querySelectorAll("header nav ul .elementos_nav a");
+console.log(navListLinks);
+
+let strongAuthors = document.querySelectorAll("#posts .post .post-autor strong")
+console.log(strongAuthors);
+
+let strongDates = document.querySelectorAll("#posts .post .post-data strong")
+console.log(strongDates);
