3c331c58d9097f92436432c124b8bf2f6284cbc6
Davi A Alves <daviih2000@gmail.com>
2023-05-03 12:54:09 -0300

    Criado elemento no DOM, elemento foi manipulado e adicionado na página.

--- criar-elementos-no-dom/assets/js/script.js
+++ criar-elementos-no-dom/assets/js/script.js
@@ -3,18 +3,35 @@ const arrayPostagens = [
     titulo: "Pop Vegan",
     subtitulo: "Comida vegana para todos!",
     data: "06/07/2022",
-    texto: "Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)"
+    texto:
+      "Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)",
   },
   {
     titulo: "Make Hommus. Not War",
     subtitulo: "Só delivery, para curtir em casa!",
     data: "18/08/2022",
-    texto: "Neste restaurante não só pode, como é encorajado comer o antepasto como prato principal. Recomendamos os kibes e a kafta bonina."
+    texto:
+      "Neste restaurante não só pode, como é encorajado comer o antepasto como prato principal. Recomendamos os kibes e a kafta bonina.",
   },
   {
     titulo: "Churrascada do Mar",
     subtitulo: "Melhor do que estar na praia!",
     data: "30/08/2022",
-    texto: "Todos conhecemos e amamos um bom churrasco, mas o que você acha de experimentar um churrasco focado em frutos do mar? Nós gostamos, experimente e nos conte o que você achou!"
+    texto:
+      "Todos conhecemos e amamos um bom churrasco, mas o que você acha de experimentar um churrasco focado em frutos do mar? Nós gostamos, experimente e nos conte o que você achou!",
   },
-]
+];
+
+// 1 - Criar um elemento:
+let article = document.createElement("article");
+
+// 2 - Manipular o elemento:
+article.innerHTML = `<h3>Pop Vegan</h3>
+<p class="subtitulo">Comida vegana para todos!</p>
+<div class="data">06/07/2022</div>
+<p>Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)</p>
+`;
+
+// 3 - Adicionar o elemento:
+let main = document.querySelector("main");
+main.appendChild(article);
